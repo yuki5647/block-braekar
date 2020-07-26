6591751138a0355b8353094f5d6178eba39d33d5
yukitosi <t.yuki.0527@gmail.com>
2020-07-27 01:05:30 +0900

    ブロックブレーカーのテストプロジェクト
    あとは終わり方を別々にするのとエフェクトをつけることをします。

---
+++ Assets/ball.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ball : MonoBehaviour {
+
+    public float speed = 5.0f;
+    private Rigidbody myRigidbody;
+
+    private float coefficient = 0.9f;
+
+    private bool isEnd = false;
+
+    private float visiblePosZ = -7f;
+
+    private GameObject GameOvertext;
+
+    private GameObject cleartext;
+
+   
+
+	// Use this for initialization
+	void Start () {
+
+        this.GameOvertext = GameObject.Find("GameOvertext");
+
+        this.cleartext = GameObject.Find("cleartext");
+
+
+
+        myRigidbody = this.GetComponent<Rigidbody>();
+        myRigidbody.AddForce((transform.forward + transform.right) * speed, ForceMode.VelocityChange);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (this.transform.position.z < this.visiblePosZ)
+        {
+            this.GameOvertext.GetComponent<Text>().text = "GameOver";
+        }
+
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+        Debug.Log(other.gameObject.tag);
+        if (other.gameObject.tag == "blocktag")
+        {
+            
+                    this.cleartext.GetComponent<Text>().text = "clear";
+                }
+            }
+        }
+    
+
---
+++ Assets/player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+    public float speed = 5.0f;
+
+
+
+    // Use this for initialization
+    void Start() {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            if (this.transform.position.x < 4.5)
+                this.transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            if (this.transform.position.x > -4.5)
+                this.transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+
+    }
+}
